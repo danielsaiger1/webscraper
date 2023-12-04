1b06d4ffc717d4cd96037265a49083b9af644757
Daniel <daniel.saiger@yahoo.de>
2023-12-04 20:49:31 +0100

    initial

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-print("hello")
-print("das ist ein Test")
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://www.stepstone.de/jobs/data-engineer'
+html_text = requests.get(url).text
+soup = BeautifulSoup(html_text, 'lxml')
+print(soup)
